2c31f0bc72533cfc35113b31502bf9515b64393e
ramit101 <amitranjan876@gmail.com>
2026-04-24 16:10:15 +0530

    added a method

--- src/main/java/com/cardekho/controller/Demo.java
+++ src/main/java/com/cardekho/controller/Demo.java
@@ -3,7 +3,8 @@ package com.cardekho.controller;
 public class Demo {
 
     public String method(){
-
+        Demo d = new Demo();
+        System.out.println("this is without add .");
         return "friend";
     }
 
